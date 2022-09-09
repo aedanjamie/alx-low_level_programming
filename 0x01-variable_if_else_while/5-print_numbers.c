889b723346101581a2ee96aa5fb4d233f0beb43b
aedanjamie <aedanjamie@gmail.com>
2022-09-09 07:51:21 -0700

    0x01

---
+++ 0x01-variable_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Print the numbers from 0 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
