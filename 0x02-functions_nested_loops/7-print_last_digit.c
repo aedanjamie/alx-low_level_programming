0608d486ec8ada3269515340e4de752b17aec3c2
aedanjamie <aedanjamie@gmail.com>
2022-09-13 14:23:44 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,16 @@
 int print_last_digit(int n)
 {
 	int l;
+	
 	l = n % 10;
 	if (l < 0)
 	{
-		_putchar(-1 + 48);
+		_putchar(-1 + 57);
 		return (-1);
 	}
 	else
 	{
-		_putchar(1 + 48);
+		_putchar(1 + 57);
 		return (1);
 	}
 }
