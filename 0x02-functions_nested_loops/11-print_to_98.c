1febb101366841b5244ac918a661365480535567
aedanjamie <aedanjamie@gmail.com>
2022-09-14 11:29:14 -0700

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * orint_to_98 - Prints all natrual numbers
+ * from n to 98
+ *
+ * @n: input number
+ *
+ * Return: no return
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for(;n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d, ", n);
+}
