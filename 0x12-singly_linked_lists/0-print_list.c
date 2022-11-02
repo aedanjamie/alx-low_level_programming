ebcf677d510887fa4b912164b59f9bf69906701b
aedanjamie <aedanjamie@gmail.com>
2022-11-02 10:32:21 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 /**
  * print_list - prints all the elements of a list_t list
  * @h: singly linked list
- * Return: the number of elements in the list
+ * Return: number of elements in the list
  */
 
 size_t print_list(const list_t *h)
