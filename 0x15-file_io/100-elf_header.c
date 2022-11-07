af24950e4877f329f5a0706fe326489398d9e84b
aedanjamie <aedanjamie@gmail.com>
2022-11-07 15:01:47 -0800

    Elf header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -17,7 +17,7 @@ void print_addr(char *ptr)
 	int begin;
 	char sys;
 
-	printf("  Entry point address:               0x");
+	printf("  Entry point address:             0x");
 
 	sys = ptr[4] + '0';
 	if (sys == '1')
