d62e9c4b0bd2813289efeec3edee446a58738a99
aedanjamie <aedanjamie@gmail.com>
2022-12-14 09:27:32 -0800

    Main

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,4 +23,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int nrandom();
 
-#endif /* MAIN_H */
+#endif /* _MAIN_H_ */
